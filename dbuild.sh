d6edefaa808a0eb4159e869cbe95da3c839f1fa8
Cloud Han <cloudhan@outlook.com>
2025-04-07 06:08:51 +0000

    backup

--- dbuild.sh
+++ dbuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd $(dirname $(realpath $0))
-base_image=$(grep 'LLM_DOCKER_IMAGE =' ~/workspaces/trtllm/jenkins/L1_Nightly.groovy  | sed 's#.*"\(.*\)"#\1#')
+base_image=$(grep 'LLM_DOCKER_IMAGE =' ~/workspaces/trtllm/jenkins/L0_MergeRequest.groovy  | sed 's#.*"\(.*\)"#\1#')
 docker build \
   -f Dockerfile.trtllm \
   --build-arg BASE_IMAGE=${base_image} \
