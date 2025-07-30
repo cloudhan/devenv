9d40e6a96157207ff3fb206e91771a2f81b8cb8e
Cloud Han <cloudhan@outlook.com>
2025-07-30 15:10:20 +0000

    backup

--- dbuild.sh
+++ dbuild.sh
@@ -1,9 +1,34 @@
 #!/bin/bash
 
 cd $(dirname $(realpath $0))
-base_image=$(grep 'LLM_DOCKER_IMAGE =' ~/workspaces/trtllm/jenkins/L0_MergeRequest.groovy  | sed 's#.*"\(.*\)"#\1#')
-docker build \
-  -f Dockerfile.trtllm \
-  --build-arg BASE_IMAGE=${base_image} \
-  -t hgy-trtllm-devimage \
-  .
+# base_image=$(grep 'LLM_DOCKER_IMAGE =' ~/workspaces/trtllm/jenkins/L0_MergeRequest.groovy  | sed 's#.*"\(.*\)"#\1#')
+
+# updage tag  https://urm.nvidia.com/artifactory/sw-tensorrt-docker/tensorrt-llm/
+base_image="urm.nvidia.com/sw-tensorrt-docker/tensorrt-llm:pytorch-25.05-py3-x86_64-ubuntu24.04-trt10.11.0.33-skip-tritondevel-202507101530-5434"
+# base_image="urm.nvidia.com/sw-tensorrt-docker/tensorrt-llm:pytorch-25.06-py3-x86_64-ubuntu24.04-trt10.11.0.33-skip-tritondevel-202507161655-5678"
+
+force_build=0
+quiet=0
+while getopts "fq" opt; do
+  case $opt in
+    f)
+      force_build=1
+      ;;
+    q)
+      quiet=1
+      ;;
+  esac
+done
+
+if [[ $force_build -eq 1 ]] || ! docker image inspect hgy-trtllm-devimage > /dev/null 2>&1; then
+  echo "Building hgy-trtllm-devimage..."
+  docker build \
+    -f Dockerfile.trtllm \
+    --build-arg BASE_IMAGE=${base_image} \
+    -t hgy-trtllm-devimage \
+    .
+else
+  if [[ $quiet -ne 1 ]]; then
+    echo "Image hgy-trtllm-devimage already exists. Use -f to force rebuild."
+  fi
+fi
--- drun.sh
+++ drun.sh
@@ -2,6 +2,8 @@
 
 cd $(dirname $(realpath $0))
 
+./dbuild.sh -q
+
 docker run \
   --runtime nvidia --gpus "device=$NV_GPU" \
   --name hgy-trtllm-devcontainer -dit --init --rm\
@@ -10,6 +12,8 @@ docker run \
   -e HGY_HOST_UID=$(id -u) \
   -e HGY_DOCKER_ENV=1 \
   --mount type=bind,source=/home,target=/home,bind-propagation=rslave \
-  --mount type=bind,source=/tmp,target=/tmp,bind-propagation=rslave \
+  --mount type=bind,source=/tmp,target=/tmp \
   hgy-trtllm-devimage \
   sleep infinity
+
+  # --mount type=bind,source=/tmp,target=/tmp,bind-propagation=rslave \
