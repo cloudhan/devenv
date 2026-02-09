be3ff8a63eb6962eb8babf100fadd659541a460d
Cloud Han <cloudhan@outlook.com>
2026-02-09 02:43:13 +0000

    update

--- dwrapper.sh
+++ dwrapper.sh
@@ -114,13 +114,13 @@ exec() {
 
     echo "Trying to exec into $CONTAINER_NAME..."
 
+    # export VSCODE_GIT_ASKPASS_NODE=${VSCODE_GIT_ASKPASS_NODE} && \
+    # export VSCODE_GIT_ASKPASS_EXTRA_ARGS=${VSCODE_GIT_ASKPASS_EXTRA_ARGS} && \
+    # export VSCODE_GIT_IPC_HANDLE=${VSCODE_GIT_IPC_HANDLE} && \
+    # export VSCODE_GIT_ASKPASS_MAIN=${VSCODE_GIT_ASKPASS_MAIN} && \
+    # export VSCODE_IPC_HOOK_CLI=${VSCODE_IPC_HOOK_CLI} && \
+    # alias code (dirname $VSCODE_GIT_ASKPASS_NODE)/bin/remote-cli/code && \
     docker exec -it "$CONTAINER_NAME" fish --init-command="\
-    export VSCODE_GIT_ASKPASS_NODE=${VSCODE_GIT_ASKPASS_NODE} && \
-    export VSCODE_GIT_ASKPASS_EXTRA_ARGS=${VSCODE_GIT_ASKPASS_EXTRA_ARGS} && \
-    export VSCODE_GIT_IPC_HANDLE=${VSCODE_GIT_IPC_HANDLE} && \
-    export VSCODE_GIT_ASKPASS_MAIN=${VSCODE_GIT_ASKPASS_MAIN} && \
-    export VSCODE_IPC_HOOK_CLI=${VSCODE_IPC_HOOK_CLI} && \
-    alias code (dirname $VSCODE_GIT_ASKPASS_NODE)/bin/remote-cli/code && \
     if test -e $PWD; cd $PWD; else; set_color red; echo '$PWD does not exist in container'; set_color normal; cd ~/workspaces; end"
 }
 
